690a09947c4ecbd2356d3758429531b580e86178
PartyPancakess <bilgehalilonayci@gmail.com>
2020-12-15 03:19:26 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,71 +1,32 @@
 [![Unix Build Status][ci-img]][ci-url]
 [![Windows Build Status][ci-win-img]][ci-win-url]
 [![Code Climate][clim-img]][clim-url]
-[![NPM][npm-img]][npm-url]
 
 # haraka-plugin-domain-aliases-etcd-config
 
-Clone me, to create a new Haraka plugin!
+This plugin recieves its config from etcd. Other than that, works the same way as domain-aliases plugin: https://github.com/PartyPancakess/haraka-plugin-domain-aliases
 
-# Template Instructions
 
-These instructions will not self-destruct after use. Use and destroy.
+& WARNING: DO NOT USE THIS PLUGIN WITH queue/smtp_proxy.
+& WARNING: This plugin must be the last rcpt_to plugin in the config/plugins list (except for rcpt_to.in_host_list and haraka-plugin-domain-limit).
 
-See also, [How to Write a Plugin](https://github.com/haraka/Haraka/wiki/Write-a-Plugin) and [Plugins.md](https://github.com/haraka/Haraka/blob/master/docs/Plugins.md) for additional plugin writing information.
+## Configuration
+#### Available actions:
+- drop
+- alias
+	- to (required)
+- domain-alias
+	- to (required)
 
-## Create a new repo for your plugin
 
-Haraka plugins are named like `haraka-plugin-something`. All the namespace after `haraka-plugin-` is yours for the taking. Please check the [Plugins](https://github.com/haraka/Haraka/blob/master/Plugins.md) page and a Google search to see what plugins already exist.
 
-Once you've settled on a name, create the GitHub repo. On the repo's main page, click the _Clone or download_ button and copy the URL. Then paste that URL into a local ENV variable with a command like this:
-
-```sh
-export MY_GITHUB_ORG=haraka
-export MY_PLUGIN_NAME=haraka-plugin-SOMETHING
+### Example etcd Configuration
 ```
-
-Clone and rename the domain-aliases-etcd-config repo:
-
-```sh
-git clone git@github.com:haraka/haraka-plugin-domain-aliases-etcd-config.git
-mv haraka-plugin-domain-aliases-etcd-config $MY_PLUGIN_NAME
-cd $MY_PLUGIN_NAME
-git remote rm origin
-git remote add origin "git@github.com:$MY_GITHUB_ORG/$MY_PLUGIN_NAME.git"
+etcdctl put config_alias_1 '"test1": { "action": "drop" }'
+etcdctl put config_alias_2 '"test2": { "action": "alias", "to": "non-test2" }'
+etcdctl put config_alias_3 '"@domain.com": { "action" : "domain-alias", "to" : "@example.com" }'
 ```
-
-Now you'll have a local git repo to begin authoring your plugin
-
-## rename boilerplate
-
-Replaces all uses of the word `domain-aliases-etcd-config` with your plugin's name.
-
-./redress.sh [something]
-
-You'll then be prompted to update package.json and then force push this repo onto the GitHub repo you've created earlier.
-
-
-# Add your content here
-
-## INSTALL
-
-```sh
-cd /path/to/local/haraka
-npm install haraka-plugin-domain-aliases-etcd-config
-echo "domain-aliases-etcd-config" >> config/plugins
-service haraka restart
-```
-
-### Configuration
-
-If the default configuration is not sufficient, copy the config file from the distribution into your haraka config dir and then modify it:
-
-```sh
-cp node_modules/haraka-plugin-domain-aliases-etcd-config/config/domain-aliases-etcd-config.ini config/domain-aliases-etcd-config.ini
-$EDITOR config/domain-aliases-etcd-config.ini
-```
-
-## USAGE
+In above example, instead of 1,2,3 after config_alias_, it is possible to put anything as long as it is "unique." Ex: config_alias_id, config_alias_id2
 
 
 <!-- leave these buried at the bottom of the document -->
@@ -76,4 +37,3 @@ $EDITOR config/domain-aliases-etcd-config.ini
 [clim-img]: https://codeclimate.com/github/haraka/haraka-plugin-domain-aliases-etcd-config/badges/gpa.svg
 [clim-url]: https://codeclimate.com/github/haraka/haraka-plugin-domain-aliases-etcd-config
 [npm-img]: https://nodei.co/npm/haraka-plugin-domain-aliases-etcd-config.png
-[npm-url]: https://www.npmjs.com/package/haraka-plugin-domain-aliases-etcd-config
--- index.js
+++ index.js
@@ -1,20 +1,180 @@
-'use strict'
+// Do not run this plugin with the queue/smtp_proxy plugin.
+// Place this plugin before rcpt_to.in_host_list in config/plugins file.
+// This plugin must be the last rcpt_to plugin in the config/plugins list (except for rcpt_to.in_host_list and haraka-plugin-domain-limit).
+const Address = require('../haraka-necessary-helper-plugins/address-rfc2821').Address;
+
+const { Etcd3 } = require('../haraka-necessary-helper-plugins/etcd3');
+const client = new Etcd3();
 
 exports.register = function () {
-  this.load_domain-aliases-etcd-config_ini()
+  this.inherits('queue/discard');
+
+  this.load_aliases();
+
+  this.register_hook('rcpt', 'aliases');
+};
+
+exports.load_aliases = function () {
+  const plugin = this;
+
+  var tempConfig = {}
+  plugin.cfg = tempConfig;
+
+  (async () => {
+      const list = await client.getAll().prefix('config_alias').strings();
+      
+      tempConfig = {};
+      for (var key in list) {
+        if (list.hasOwnProperty(key)) {
+          var from = list[key].substr(0, list[key].indexOf(":"));
+          var to = list[key].substr(list[key].indexOf(":") + 1);
+          tempConfig[JSON.parse(from)] = JSON.parse(to);
+        }
+      }
+
+      plugin.cfg = tempConfig;
+    })();
+
+    client.watch()
+    .prefix('config_alias')
+    .create()
+    .then(watcher => {
+      watcher
+        .on('disconnected', () => console.log('disconnected...'))
+        .on('connected', () => console.log('successfully reconnected!'))
+        .on('put', res => {
+          var from = res.value.toString().substr(1, res.value.toString().indexOf(":")-2);
+          var to = res.value.toString().substr(res.value.toString().indexOf(":") + 2);
+
+          tempConfig[from] = JSON.parse(to);
+          plugin.cfg = tempConfig;
+        });
+    });
+
 }
 
-exports.load_domain-aliases-etcd-config_ini = function () {
-  const plugin = this
+exports.aliases = function (next, connection, params) {
+  const plugin = this;
+  const cfg = plugin.cfg;
+  const rcpt = params[0].address();
+  const user = params[0].user;
+  const host = params[0].host;
 
-  plugin.cfg = plugin.config.get('domain-aliases-etcd-config.ini', {
-    booleans: [
-      '+enabled',               // plugin.cfg.main.enabled=true
-      '-disabled',              // plugin.cfg.main.disabled=false
-      '+feature_section.yes'    // plugin.cfg.feature_section.yes=true
-    ]
-  },
-  function () {
-    plugin.load_example_ini()
-  })
+  let match = user.split(/[+-]/, 1);
+  let action = "<missing>";
+
+  function onMatch(match1, action1) {
+    switch (action.toLowerCase()) {
+      case 'drop':
+        _drop(plugin, connection, match1);
+        break;
+      case 'alias':
+        _alias(plugin, connection, match1, cfg[match1], host, next);
+        return;
+      case 'domain-alias':
+        _domain_alias(plugin, connection, match1, cfg[match1], host, next);
+        return;
+      default:
+        connection.loginfo(plugin, "unknown action: " + action1);
+    }
+    next();
+  }
+
+  // full email address match
+  if (cfg[rcpt]) {
+    if (cfg[rcpt].action) action = cfg[rcpt].action;
+    return onMatch(rcpt, action);
+  }
+
+  // user only match
+  if (cfg[user]) {
+    if (cfg[user].action) action = cfg[user].action;
+    return onMatch(user, action);
+  }
+
+  // user prefix match
+  if (cfg[match[0]]) {
+    if (cfg[match[0]].action) action = cfg[match[0]].action;
+    return onMatch(match[0], action);
+  }
+
+  // user prefix + domain match
+  const prefix_dom = `${match[0]}@${host}`;
+  if (cfg[prefix_dom]) {
+    if (cfg[prefix_dom].action) action = cfg[prefix_dom].action;
+    return onMatch(prefix_dom, action);
+  }
+
+  // @domain match
+  const dom_match = `@${host}`;
+  if (cfg[`@${host}`]) {
+    if (cfg[dom_match].action) action = cfg[dom_match].action;
+    match = dom_match;
+    return onMatch(dom_match, action);
+  }
+
+  next();
+};
+
+function _drop(plugin, connection, rcpt) {
+  connection.logdebug(plugin, "marking " + rcpt + " for drop");
+  connection.transaction.notes.discard = true;
+}
+
+function _alias(plugin, connection, key, config, host, next) {
+  const txn = connection.transaction;
+
+  if (!config.to) {
+    connection.loginfo(plugin, `alias failed for ${key}, no "to" field in alias config`);
+    return next();
+  }
+
+  if (Array.isArray(config.to)) {
+    connection.logdebug(plugin, `aliasing ${txn.rcpt_to} to ${config.to}`);
+    txn.rcpt_to.pop();
+    config.to.forEach((addr) => {
+      txn.rcpt_to.push(new Address(`<${addr}>`));
+    })
+    return next();
+  }
+
+  let to = config.to;
+  if (to.search("@") === -1) {
+    to = config.to + '@' + host;
+  }
+
+  connection.logdebug(plugin, "aliasing " + txn.rcpt_to + " to " + to);
+  const original_rcpt = txn.rcpt_to.pop();
+  txn.rcpt_to.push(new Address(`<${to}>`));
+
+  return next(OK, `recipient ${original_rcpt} OK`);
+}
+
+
+function _domain_alias(plugin, connection, key, config, host, next) {
+  const txn = connection.transaction;
+
+  if (!config.to) {
+    connection.loginfo(plugin, `domain alias failed for ${key}, no "to" field in config`);
+    return next();
+  }
+  
+
+  let to = config.to;
+  if (to.search("@") !== 0 || key.search("@") !== 0) {
+    connection.loginfo(plugin, `domain-alias failed for ${key}, domain field is not accepted! Please fix the config file of the plugin.
+    Correct form: "@example.com" : { "action" : "domain-alias", "to" : "@domain.test" }
+    Continuing  without changing domain.`);
+    // throw new Error('');
+    return next();
+  }
+
+  
+  connection.logdebug(plugin, "domain-aliasing " + txn.rcpt_to + " to " + to);
+  const original_rcpt = txn.rcpt_to.pop();
+  to = original_rcpt.user + to;
+  
+  txn.rcpt_to.push(new Address(`<${to}>`));
+
+  return next(OK, `recipient ${original_rcpt} OK`);
 }
